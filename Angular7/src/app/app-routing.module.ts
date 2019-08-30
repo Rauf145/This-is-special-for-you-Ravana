23c87560f3f9087a188008a7196481066f47f027
Ravana Safaraliyeva <s.ravana@Ravana-Air.mshome.net>
2019-08-30 10:44:57 +0400

    the last version

--- Angular7/src/app/app-routing.module.ts
+++ Angular7/src/app/app-routing.module.ts
@@ -1,3 +1,7 @@
+import { UsersComponent } from './home/appmenu/users/users.component';
+import { ReportsComponent } from './home/appmenu/reports/reports.component';
+import { MessagesComponent } from './home/appmenu/messages/messages.component';
+import { MonitoringComponent } from './home/appmenu/monitoring/monitoring.component';
 import { AuthGuard } from './auth/auth.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -5,6 +9,7 @@ import { UserComponent } from './user/user.component';
 import { RegistrationComponent } from './user/registration/registration.component';
 import { LoginComponent } from './user/login/login.component';
 import { HomeComponent } from './home/home.component';
+import { TracksComponent } from './home/appmenu/tracks/tracks.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'/user/login',pathMatch:'full'},
@@ -15,7 +20,16 @@ const routes: Routes = [
       { path: 'login', component: LoginComponent }
     ]
   },
-  {path:'home',component:HomeComponent}
+  {
+    path:'home',component:HomeComponent,
+    children: [
+      {path:'tracks',component:TracksComponent},
+      {path:'monitoring',component:MonitoringComponent},
+      {path:'messages',component:MessagesComponent},
+      {path:'reports',component:ReportsComponent},
+      {path:'users',component:UsersComponent}
+    ]
+  }
 ];
 
 @NgModule({
